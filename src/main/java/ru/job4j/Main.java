f2eb7781af8375ea44e4ecf5f0790c30e07a17bc
nickzapryagaev <impulse.nn@gmail.com>
2022-10-10 21:57:32 +0600

    9.1. String [#237407]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,5 +3,8 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, Мир!");
+        int age = 18;
+        double bacterium = 10;
+        System.out.println(bacterium);
     }
 }
